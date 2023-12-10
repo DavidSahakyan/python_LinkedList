d22c2c14d7a8c0a75e1b25a8cdbdef01237aaaaf
DavidSahakyan <123825441+DavidSahakyan@users.noreply.github.com>
2023-12-10 14:45:51 +0400

    Add files via upload

--- LinkedList.py
+++ LinkedList.py
@@ -2,96 +2,91 @@ from My_Node import Node
 
 class LinkedList:
     def __init__(self, *args):
-        self._n = Node()
-        self._len = 0
+        self.__n = Node()
+        self.__len = 0
         
         for i in args:
             self.append(i)
             
         
     def append(self, a):
-        temp = self._n
+        temp = self.__n
         
-        if self._len == 0:
-            self._n._a = a
-            self._len += 1
+        if self.__len == 0:
+            self.__n.set_a(a)
+            self.__len += 1
             return
         
-        while temp._next != None:
-            temp = temp._next
+        while temp.get_next() != None:
+            temp = temp.get_next()
         
-        self._len += 1    
-        temp._next = Node(a, self)
+        self.__len += 1    
+        temp.set_next(Node(a, self))
         
     def prepend(self, a):
-        self._n._prev = Node(a, None, self._n)
-        self._n = self._n._prev
-        self._len += 1  
+        self.__n.set_prev(Node(a, None, self.__n))
+        self.__n = self.__n.get_prev()
+        self.__len += 1  
         
     def insert_after(self, target_data, a):
-        temp = self._n
+        temp = self.__n
         
         try:
-            while temp._a != target_data:
-                temp = temp._next
+            while temp.get_a() != target_data:
+                temp = temp.get_next()
         except(AttributeError):
             return
         
-        temp_node = temp._next
-        temp._next = Node(a, temp, temp_node)
-        self._len += 1
+        temp_node = temp.get_next()
+        temp.set_next(Node(a, temp, temp_node))
+        self.__len += 1
     
     def insert_before(self, target_data, data):
-        temp = self._n
+        temp = self.__n
+        
+        if self.__n.get_a() == target_data:
+            self.prepend(data)
+            return
+            
         
         try:
-            while temp._a != target_data:
-                temp = temp._next
+            while temp.get_next().get_a() != target_data:
+                temp = temp.get_next()
         except(AttributeError):
             return
         
-        temp._prev._next = Node(data, temp._prev, temp)
-        self._len += 1
+        temp.set_next(Node(data, temp, temp.get_next()))
+                        
+        self.__len += 1
         
     def delete(self, data):
-        temp = self._n
+        temp = self.__n
         
-        if self._n._a == data:
-            self._n = self._n._next
-            self._n._prev = None
-            self._len -= 1
+        if self.__n.get_a() == data:
+            self.__n = self.__n.get_next()
+            self.__n.set_prev(None)
+            self.__len -= 1
             return
         
         try:
-            while temp._a != data:
-                temp = temp._next
+            while temp.get_a() != data:
+                temp = temp.get_next()
         except(AttributeError):
             return
        
-        temp._prev._next = temp._next
-        self._len -= 1
+        temp.get_prev().set_next(temp.get_next())  
+        self.__len -= 1
         
         
-    def display(self):
-        temp = self._n
+    def display(self, sep = '\n'):
+        temp = self.__n
         
         while temp != None:
-            print(temp._a)
-            temp = temp._next
+            print(temp.get_a(), sep = sep)
+            temp = temp.get_next()
             
     def is_empty(self):
-        return not bool(self._len)
+        return not bool(self.__len)
     
     def length(self):
-        return self._len
-    
-    
-a = LinkedList()
-
-a.append(1)
-a.prepend(1)
-a.insert_after(1, 4)
-a.delete(4)
-a.append(1)
-
-a.display()
+        return self.__len
--- My_Node.py
+++ My_Node.py
@@ -1,6 +1,24 @@
 class Node:
     def __init__(self, a = None, prev = None, next = None):
-        self._prev = prev
-        self._next = next
-        self._a = a
+        self.__prev = prev
+        self.__next = next
+        self.__a = a
+        
+    def get_a(self):
+        return self.__a
+    
+    def get_prev(self):
+        return self.__prev
+    
+    def get_next(self):
+        return self.__next
+    
+    def set_a(self, a):
+        self.__a = a
+    
+    def set_prev(self, prev):
+        self.__prev = prev
+    
+    def set_next(self, next):
+        self.__next = next
         
